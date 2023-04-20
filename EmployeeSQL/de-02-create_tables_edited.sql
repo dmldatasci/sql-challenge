69da15fd3d146053fb763fb6a64990a311cee19c
David Levy <dmldatasci@gmail.com>
2023-04-20 01:31:32 -0600

    Edited data modeling files.

--- EmployeeSQL/de-02-create_tables_edited.sql
+++ EmployeeSQL/de-02-create_tables_edited.sql
@@ -7,16 +7,6 @@ DROP TABLE IF EXISTS dept_manager;
 DROP TABLE IF EXISTS departments;
 
 -- create tables corresponding with each csv file provided by Pewlett Hackard
-CREATE TABLE employees (
-	emp_no INT PRIMARY KEY,
-	emp_title_id VARCHAR(5) FOREIGN KEY,
-	birth_date DATE,
-	first_name VARCHAR(30) NOT NULL,
-	last_name VARCHAR (30) NOT NULL,
-	sex VARCHAR(1),
-	hire_date DATE NOT NULL
-);
-
 CREATE TABLE titles (
 	title_id VARCHAR(5) PRIMARY KEY,
 	title VARCHAR(20) NOT NULL
@@ -25,6 +15,19 @@ CREATE TABLE titles (
 CREATE TABLE departments (
 	dept_no VARCHAR(5) PRIMARY KEY,
 	dept_name VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE employees (
+	emp_no INT PRIMARY KEY,
+	emp_title_id VARCHAR(5) FOREIGN KEY,
+	birth_date DATE,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR (30) NOT NULL,
+	sex VARCHAR(1),
+	hire_date DATE NOT NULL
+	
+	CONSTRAINT FK_emp_title FOREIGN KEY(emp_title_id)
+		REFERENCES titles(title_id)
 );
 
 CREATE TABLE salaries (
